3ba651c58440dc4c177c6483734ce3151d675494
INTERNATIONALES WEB SERVICES <franckagbokoudjo301@gmail.com>
2025-07-31 03:46:42 +0100

    custom bundle please don't merge with future SonataDoctrineOrmBundle version 5.0

--- src/Model/ModelManager.php
+++ src/Model/ModelManager.php
@@ -11,6 +11,18 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+/*
+ * This file is part of the project by AGBOKOUDJO Franck.
+ *
+ * (c) AGBOKOUDJO Franck <internationaleswebservices@gmail.com>
+ * Phone: +229 01 67 25 18 86
+ * LinkedIn: https://www.linkedin.com/in/internationales-web-apps-services-120520193/
+ * Github: https://github.com/Agbokoudjo/
+ * Company: INTERNATIONALES WEB APPS & SERVICES
+ *
+ * For more information, please feel free to contact the author.
+ */
+
 namespace Sonata\DoctrineORMAdminBundle\Model;
 
 use Doctrine\DBAL\Exception;
